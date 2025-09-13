f36ab359ed398113bfead5ac6a05e3add0ae8725
Johan Gonzalez <johangonzalez790@gmail.com>
2025-09-13 00:34:50 -0300

    configuracion de dockerfile para tener los servicios separados

--- backend/migration_fix_fecha_verificacion.sql
+++
@@ -1,8 +0,0 @@
--- Migración para permitir NULL en fecha_verificacion
--- Ejecutar este script en la base de datos para corregir la estructura
-
--- Cambiar la columna fecha_verificacion para permitir NULL
-ALTER TABLE perfil_empresa ALTER COLUMN fecha_verificacion DROP NOT NULL;
-
--- Comentario explicativo
-COMMENT ON COLUMN perfil_empresa.fecha_verificacion IS 'Fecha cuando el administrador aprobó la verificación. NULL si aún no ha sido verificada.';
