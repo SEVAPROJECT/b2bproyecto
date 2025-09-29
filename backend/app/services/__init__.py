2f340b466be85dbe7f1f0a316939957003c4bb64
Johan Gonzalez <johangonzalez790@gmail.com>
2025-09-28 21:07:03 -0300

    se borro la carpeta supabase_client

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -2,7 +2,7 @@
 Módulo de servicios de la aplicación
 """
 
-from .perfil_empresa_service import PerfilEmpresaService
+# from .perfil_empresa_service import PerfilEmpresaService  # Comentado temporalmente para evitar importación circular
 from .direct_password_reset import DirectPasswordResetService
 from .email_service import EmailService
 from .gmail_smtp_service import GmailSMTPService
@@ -11,7 +11,7 @@ from .supabase_password_reset import SupabasePasswordResetService
 from .date_service import DateService
 
 __all__ = [
-    "PerfilEmpresaService",
+    # "PerfilEmpresaService",  # Comentado temporalmente
     "DirectPasswordResetService", 
     "EmailService",
     "GmailSMTPService",
